09aae7287fbc0bb887b31956dffe132dfb0a66a5
1Lukukas <88420914+1Lukukas@users.noreply.github.com>
2022-04-18 21:20:25 +0300

    cringe

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/cringe')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended : true}))
+app.get("/", (req, res) => {
+    res.render("index.ejs");
 });
+
 app.listen(3000, function () {
     console.log("Server is running on localhost3000");
 });
