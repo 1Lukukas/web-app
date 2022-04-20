01028928df2022f669c34ede7d1c718386a18dbd
1Lukukas <88420914+1Lukukas@users.noreply.github.com>
2022-04-20 18:22:44 +0300

    debug

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
-const { Router } = require('express');
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const router = require('./routes/records');
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 
@@ -13,11 +13,18 @@ db.once('open', () => console.log('Connected to Database'))
 app.use(express.json())
 const recordsRouter = require('./routes/records')
 app.use('/records', recordsRouter)
-
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended : true}))
+
+data = app.get('records/all', (req, res) => {
+}).res;
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.send(`${data}`);
 });
+// app.get("/", (req, res) => {
+//     res.render("index.ejs", {data : data,}
+// )})
 app.use("/static", express.static('./static/'));
 app.listen(3000, ( ) => {
     console.log("Server is running on localhost3000");
