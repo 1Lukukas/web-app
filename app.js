c7673d02479e29b549482874de51383773e436cf
Edvinas Gaubas <e.gaubas18@gmail.com>
2022-04-21 19:31:04 +0300

    Switch to https

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const router = require('./routes/records');
 const methodOverride = require('method-override');
+const https = require('https');
+const fs = require('fs');
+const key = fs.readFileSync('key.pem');
+const cert = fs.readFileSync('cert.pem');
+const server = https.createServer({key: key, cert: cert }, app);
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
@@ -21,7 +27,7 @@ app.set('view engine', 'ejs');
 
 const getAllRecords = async (req, res, next) => {
     axios = require('axios');
-    response = await axios.get('http://localhost:3000/records/all')
+    response = await axios.get('https://localhost:3000/records/all')
     req.data = response.data
     next()
 }
@@ -35,7 +41,4 @@ app.get("/", (req, res) => {
     res.render("index.ejs", {data : req.data})
 })
 
-app.use("/static", express.static('./static/'));
-app.listen(3000, ( ) => {
-    console.log("Server is running on localhost3000");
-});
+server.listen(3000, () => { console.log('listening on 3000') });
