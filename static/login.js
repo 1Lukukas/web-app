28191520407ecf6fdf50a3ee2aa3cc164099c2ad
1Lukukas <88420914+1Lukukas@users.noreply.github.com>
2022-04-30 16:40:55 +0300

    EM-17 add frontend login validation

--- static/login.js
+++ static/login.js
@@ -1,12 +1,54 @@
+async function login() {
+	if (await validateForm()) {
+		const response = await axios.post('https://localhost:3000/users/login', {
+			username: document.getElementById("username").value
+		})
+		window.localStorage.setItem('access_token', response.data.accessToken);
+		location.href = "https://localhost:3000/lol"
+	}
+}
 
 async function validateForm() {
-    //document.getElementById("loginForm").submit()
-    //validation
-    
-    const response = await axios.post('https://localhost:3000/users/login',
-        {
-            username: document.getElementById("username").value
-        })
-    window.localStorage.setItem('access_token', response.data.accessToken);
-    location.href="https://localhost:3000/lol"
+	if (document.getElementById("username").value == "" ||
+		document.getElementById("password").value == "") {
+		alert("All fields must be filled out");
+	} else if (await validateLogin()) {
+        return true
+	}
+	return false;
 }
+
+async function validateLogin() {
+
+	response = await axios.get('https://localhost:3000/users/all')
+
+	usernameExistance = response.data.filter(
+		function(data) {
+			return data.username == document.getElementById("username").value
+		}
+	)
+    password = document.getElementById("password").value
+	if (usernameExistance.length == 0 || usernameExistance[0].password != password) {
+		alert("Username or password wrong")
+		return false;
+	} else {
+		return true;
+	}
+}
+
+// async function validatePassword() {
+// 	response = await axios.get('https://localhost:3000/users/all')
+// 	usernameExistance = response.data.filter(
+// 		function(data) {
+// 			return data.username == document.getElementById("username").value
+// 		}
+// 	)
+//     console.log(usernameExistance)
+// 	password = document.getElementById("password").value
+// 	if (usernameExistance[0].password == password) {
+// 		return true
+// 	} else {
+// 		alert("Username or password wrong")
+// 		return false
+// 	}
+// }
