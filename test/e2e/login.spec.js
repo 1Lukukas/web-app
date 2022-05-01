99f45bfcb6b5d0c186cb4b3aceef9cca1beac0e6
Edvinas Gaubas <e.gaubas18@gmail.com>
2022-05-02 00:41:07 +0300

    Cleanup

--- test/e2e/login.spec.js
+++ test/e2e/login.spec.js
@@ -1,126 +1,108 @@
-require('dotenv').config()
-const { test, expect } = require('@playwright/test');
-const { default: mongoose } = require('mongoose');
+// require('dotenv').config()
+// const { test, expect } = require('@playwright/test');
+// const { default: mongoose } = require('mongoose');
 
-const User = require('../../models/user');
+// const User = require('../../models/user');
 
-test.describe('basic test', () => {
+// test.describe('Login', () => {
 
-  test.beforeAll(async() => {
-    mongoose.connect(process.env.TEST_DATABASE_URL, { useNewUrlParser: true })
-    const db = mongoose.connection
-    db.on('error', (error) => console.error(error))
-    db.once('open', () => console.log('Connected to Database'))
+//   test('check that empty field validation works', async ({ page }) => {
+//     await page.goto('https://localhost:3000/');
 
-    await new User({
-      email: "email",
-      username: "username",
-      password: "test",
-    }).save()
-  })
+//     await page.locator('id=login').click();
+//     let dialogPromise;
+//     let text;
+//     await Promise.all([
+//       dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+//         dialog.accept()
+//         return dialog.message();
+//       }),
+//       page.locator('button:has-text("LOGIN")').click(),
+//       text = await dialogPromise
+//     ])
 
-  test.afterAll(async() => {
-    await User.deleteMany({})
-    mongoose.disconnect()
-  })
+//     expect(text).toBe('All fields must be filled out')
+//   })
 
-  test('check that empty field validation works', async ({ page }) => {
-    await page.goto('https://localhost:3000/');
+//   test('check that non existing user validation works', async ({ page }) => {
+//     await page.goto('https://localhost:3000/');
+//     await page.locator('id=login').click();
 
-    await page.locator('id=login').click();
-    let dialogPromise;
-    let text;
-    await Promise.all([
-      dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
-        dialog.accept()
-        return dialog.message();
-      }),
-      page.locator('button:has-text("LOGIN")').click(),
-      text = await dialogPromise
-    ])
+//     await page.locator('input#login').fill('password');
+//     await page.locator('input#password').fill('password');
 
-    expect(text).toBe('All fields must be filled out')
-  })
+//     let dialogPromise;
+//     let text;
+//     await Promise.all([
+//       dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+//         dialog.accept()
+//         return dialog.message();
+//       }),
+//       page.locator('button:has-text("LOGIN")').click(),
+//       text = await dialogPromise
+//     ])
+//     expect(text).toBe('Username or password wrong')
+//   })
 
-  test('check that non existing user validation works', async ({ page }) => {
-    await page.goto('https://localhost:3000/');
-    await page.locator('id=login').click();
+//   test('check that wrong password validation works', async ({ page }) => {
+//     await page.goto('https://localhost:3000/');
+//     await page.locator('id=login').click();
 
-    await page.locator('input#login').fill('password');
-    await page.locator('input#password').fill('password');
+//     await page.locator('input#login').fill('username');
+//     await page.locator('input#password').fill('username');
 
-    let dialogPromise;
-    let text;
-    await Promise.all([
-      dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
-        dialog.accept()
-        return dialog.message();
-      }),
-      page.locator('button:has-text("LOGIN")').click(),
-      text = await dialogPromise
-    ])
-    expect(text).toBe('Username or password wrong')
-  })
+//     let dialogPromise;
+//     let text;
+//     await Promise.all([
+//       dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+//         dialog.accept()
+//         return dialog.message();
+//       }),
+//       page.locator('button:has-text("LOGIN")').click(),
+//       text = await dialogPromise
+//     ])
+//     expect(text).toBe('Username or password wrong')
+//   })
 
-  test('check that wrong password validation works', async ({ page }) => {
-    await page.goto('https://localhost:3000/');
-    await page.locator('id=login').click();
+//   test('check that login using username works', async ({ page }) => {
+//     await page.goto('https://localhost:3000/');
+//     await page.locator('id=login').click();
 
-    await page.locator('input#login').fill('username');
-    await page.locator('input#password').fill('username');
+//     await page.locator('input#login').fill('username');
+//     await page.locator('input#password').fill('password');
 
-    let dialogPromise;
-    let text;
-    await Promise.all([
-      dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
-        dialog.accept()
-        return dialog.message();
-      }),
-      page.locator('button:has-text("LOGIN")').click(),
-      text = await dialogPromise
-    ])
-    expect(text).toBe('Username or password wrong')
-  })
+//     let dialogPromise;
+//     let text;
+//     await Promise.all([
+//       dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+//         dialog.accept()
+//         return dialog.message();
+//       }),
+//       page.locator('button:has-text("LOGIN")').click(),
+//       text = await dialogPromise
+//     ])
+//     expect(text).toBe('Username or password wrong')
+//   })
 
-  test('check that login using username works', async ({ page }) => {
-    await page.goto('https://localhost:3000/');
-    await page.locator('id=login').click();
+//   test('check that login using email works', async ({ page }) => {
+//     await page.goto('https://localhost:3000/');
+//     await page.locator('id=login').click();
 
-    await page.locator('input#login').fill('username');
-    await page.locator('input#password').fill('password');
+//     await page.waitForSelector('#emailOption')
+//     await page.locator('#emailOption').check();
+//     await page.locator('input#login').fill('email');
+//     await page.locator('input#password').fill('password');
 
-    let dialogPromise;
-    let text;
-    await Promise.all([
-      dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
-        dialog.accept()
-        return dialog.message();
-      }),
-      page.locator('button:has-text("LOGIN")').click(),
-      text = await dialogPromise
-    ])
-    expect(text).toBe('Username or password wrong')
-  })
-
-  test('check that login using email works', async ({ page }) => {
-    await page.goto('https://localhost:3000/');
-    await page.locator('id=login').click();
-
-    await page.waitForSelector('#emailOption')
-    await page.locator('#emailOption').check();
-    await page.locator('input#login').fill('email');
-    await page.locator('input#password').fill('password');
-
-    let dialogPromise;
-    let text;
-    await Promise.all([
-      dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
-        dialog.accept()
-        return dialog.message();
-      }),
-      page.locator('button:has-text("LOGIN")').click(),
-      text = await dialogPromise
-    ])
-    expect(text).toBe('Email or password wrong')
-  })
-});
+//     let dialogPromise;
+//     let text;
+//     await Promise.all([
+//       dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+//         dialog.accept()
+//         return dialog.message();
+//       }),
+//       page.locator('button:has-text("LOGIN")').click(),
+//       text = await dialogPromise
+//     ])
+//     expect(text).toBe('Email or password wrong')
+//   })
+// });
--- test/e2e/register.spec.js
+++ test/e2e/register.spec.js
@@ -4,134 +4,258 @@ const { test, expect } = require('@playwright/test');
 const { default: mongoose} = require('mongoose');
 const User = require('../../models/user');
 
-test.describe('Registration', () => { 
-    test.beforeEach(async ({ page }) => {
-      await page.goto('https://localhost:3000/');
-      await page.locator('text=REGISTER').click();
-    })
+test.describe('Test suite', () =>{
 
+  test.beforeAll(async () =>{
+    mongoose.connect(process.env.TEST_DATABASE_URL, { useNewUrlParser: true })
+    const db = mongoose.connection
+    db.on('error', (error) => console.error(error))
+    db.once('open', () => console.log('Connected to Database'))
+  })
+
+  test.afterAll(async() => {
+    await User.deleteMany({})
+    mongoose.disconnect()
+  })
+
+  test.describe('Registration', () => { 
+      test.beforeEach(async ({ page }) => {
+        await page.goto('https://localhost:3000/');
+        await page.locator('text=REGISTER').click();
+      })
+
+      test.beforeAll(async () =>{
+        await new User({
+          email: "email",
+          username: "username",
+          password: "password",
+        }).save()
+      })
+      test.afterAll(async() => {
+        await User.deleteMany({})
+      })
+
+      test('empty fields', async ({ page }) => {
+        
+        let dialogPromise;
+        let text;
+        await Promise.all([
+        dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+          dialog.accept()
+          return dialog.message();
+        }),
+        page.locator('button:has-text("Register")').click(),
+        text = await dialogPromise
+      ])
+        expect(text).toBe("All fields must be filled out")
+      });
+
+      test('correct fields', async ({ page }) =>{
+        await page.locator('input[name="email"]').click();
+        await page.locator('input[name="email"]').fill('test');
+        await page.locator('input[name="username"]').click();
+        await page.locator('input[name="username"]').fill('test');
+        await page.locator('input[name="password"]').click();
+        await page.locator('input[name="password"]').fill('test');
+        await page.locator('input[name="repassword"]').click();
+        await page.locator('input[name="repassword"]').fill('test');
+
+        let dialogPromise;
+        let text;
+        await Promise.all([
+        dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+          dialog.accept()
+          return dialog.message();
+        }),
+        page.locator('button:has-text("Register")').click(),
+        text = await dialogPromise
+      ])
+        expect(text).toBe("Registration successful!")
+      })
+
+      test('password mismatch', async ({ page }) =>{
+        await page.locator('input[name="email"]').click();
+        await page.locator('input[name="email"]').fill('test');
+        await page.locator('input[name="username"]').click();
+        await page.locator('input[name="username"]').fill('test');
+        await page.locator('input[name="password"]').click();
+        await page.locator('input[name="password"]').fill('test');
+        await page.locator('input[name="repassword"]').click();
+        await page.locator('input[name="repassword"]').fill('te');
+
+        let dialogPromise;
+        let text;
+        await Promise.all([
+        dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+          dialog.accept()
+          return dialog.message();
+        }),
+        page.locator('button:has-text("Register")').click(),
+        text = await dialogPromise
+        ])
+        expect(text).toBe("Passwords do not match")
+      })
+
+      test('username already exists', async ({ page }) =>{
+        await page.locator('input[name="email"]').click();
+        await page.locator('input[name="email"]').fill('emailo');
+        await page.locator('input[name="username"]').click();
+        await page.locator('input[name="username"]').fill('username');
+        await page.locator('input[name="password"]').click();
+        await page.locator('input[name="password"]').fill('password');
+        await page.locator('input[name="repassword"]').click();
+        await page.locator('input[name="repassword"]').fill('password');
+        await page.pause()
+        let dialogPromise;
+        let text;
+        await Promise.all([
+        dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+          dialog.accept()
+          return dialog.message();
+        }),
+        page.locator('button:has-text("Register")').click(),
+        text = await dialogPromise
+      ])
+        expect(text).toBe("Username or email already exists")
+      })
+      
+      test('email already exists', async ({ page }) =>{
+        await page.locator('input[name="email"]').click();
+        await page.locator('input[name="email"]').fill('email');
+        await page.locator('input[name="username"]').click();
+        await page.locator('input[name="username"]').fill('usernameo');
+        await page.locator('input[name="password"]').click();
+        await page.locator('input[name="password"]').fill('password');
+        await page.locator('input[name="repassword"]').click();
+        await page.locator('input[name="repassword"]').fill('password');
+
+        let dialogPromise;
+        let text;
+        await Promise.all([
+        dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+          dialog.accept()
+          return dialog.message();
+        }),
+        page.locator('button:has-text("Register")').click(),
+        text = await dialogPromise
+      ])
+        expect(text).toBe("Username or email already exists")
+      })
+  })
+
+  test.describe('Login', () => {
 
     test.beforeAll(async () =>{
-      mongoose.connect(process.env.TEST_DATABASE_URL, { useNewUrlParser: true })
-      const db = mongoose.connection
-      db.on('error', (error) => console.error(error))
-      db.once('open', () => console.log('Connected to Database'))
       await new User({
-        email: "a",
-        username: "d",
-        password: "c",
+        email: "email",
+        username: "username",
+        password: "password",
       }).save()
+
     })
     test.afterAll(async() => {
       await User.deleteMany({})
-      mongoose.disconnect()
     })
 
-    test('empty fields', async ({ page }) => {
-      
+    test('check that empty field validation works', async ({ page }) => {
+      await page.goto('https://localhost:3000/');
+  
+      await page.locator('id=login').click();
       let dialogPromise;
       let text;
       await Promise.all([
-      dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
-        dialog.accept()
-        return dialog.message();
-      }),
-      page.locator('button:has-text("Register")').click(),
-      text = await dialogPromise
-    ])
-      expect(text).toBe("All fields must be filled out")
-    });
-
-    test('correct fields', async ({ page }) =>{
-      await page.locator('input[name="email"]').click();
-      await page.locator('input[name="email"]').fill('test');
-      await page.locator('input[name="username"]').click();
-      await page.locator('input[name="username"]').fill('test');
-      await page.locator('input[name="password"]').click();
-      await page.locator('input[name="password"]').fill('test');
-      await page.locator('input[name="repassword"]').click();
-      await page.locator('input[name="repassword"]').fill('test');
-
-      let dialogPromise;
-      let text;
-      await Promise.all([
-      dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
-        dialog.accept()
-        return dialog.message();
-      }),
-      page.locator('button:has-text("Register")').click(),
-      text = await dialogPromise
-    ])
-      expect(text).toBe("Registration successful!")
-    })
-
-    test('password mismatch', async ({ page }) =>{
-      await page.locator('input[name="email"]').click();
-      await page.locator('input[name="email"]').fill('test');
-      await page.locator('input[name="username"]').click();
-      await page.locator('input[name="username"]').fill('test');
-      await page.locator('input[name="password"]').click();
-      await page.locator('input[name="password"]').fill('test');
-      await page.locator('input[name="repassword"]').click();
-      await page.locator('input[name="repassword"]').fill('te');
-
-      let dialogPromise;
-      let text;
-      await Promise.all([
-      dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
-        dialog.accept()
-        return dialog.message();
-      }),
-      page.locator('button:has-text("Register")').click(),
-      text = await dialogPromise
+        dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+          dialog.accept()
+          return dialog.message();
+        }),
+        page.locator('button:has-text("LOGIN")').click(),
+        text = await dialogPromise
       ])
-      expect(text).toBe("Passwords do not match")
+  
+      expect(text).toBe('All fields must be filled out')
     })
-
-    test('username already exists', async ({ page }) =>{
-      await page.locator('input[name="email"]').click();
-      await page.locator('input[name="email"]').fill('b');
-      await page.locator('input[name="username"]').click();
-      await page.locator('input[name="username"]').fill('d');
-      await page.locator('input[name="password"]').click();
-      await page.locator('input[name="password"]').fill('password');
-      await page.locator('input[name="repassword"]').click();
-      await page.locator('input[name="repassword"]').fill('password');
-
+  
+    test('check that non existing user validation works', async ({ page }) => {
+      await page.goto('https://localhost:3000/');
+      await page.locator('id=login').click();
+  
+      await page.locator('input#login').fill('password');
+      await page.locator('input#password').fill('password');
+  
       let dialogPromise;
       let text;
       await Promise.all([
-      dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
-        dialog.accept()
-        return dialog.message();
-      }),
-      page.locator('button:has-text("Register")').click(),
-      text = await dialogPromise
-    ])
-      expect(text).toBe("Username or email already exists")
+        dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+          dialog.accept()
+          return dialog.message();
+        }),
+        page.locator('button:has-text("LOGIN")').click(),
+        text = await dialogPromise
+      ])
+      expect(text).toBe('Username or password wrong')
     })
-    
-    test('email already exists', async ({ page }) =>{
-      await page.locator('input[name="email"]').click();
-      await page.locator('input[name="email"]').fill('a');
-      await page.locator('input[name="username"]').click();
-      await page.locator('input[name="username"]').fill('c');
-      await page.locator('input[name="password"]').click();
-      await page.locator('input[name="password"]').fill('password');
-      await page.locator('input[name="repassword"]').click();
-      await page.locator('input[name="repassword"]').fill('password');
-
+  
+    test('check that wrong password validation works', async ({ page }) => {
+      await page.goto('https://localhost:3000/');
+      await page.locator('id=login').click();
+  
+      await page.locator('input#login').fill('username');
+      await page.locator('input#password').fill('username');
+  
       let dialogPromise;
       let text;
       await Promise.all([
-      dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
-        dialog.accept()
-        return dialog.message();
-      }),
-      page.locator('button:has-text("Register")').click(),
-      text = await dialogPromise
-    ])
-      expect(text).toBe("Username or email already exists")
+        dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+          dialog.accept()
+          return dialog.message();
+        }),
+        page.locator('button:has-text("LOGIN")').click(),
+        text = await dialogPromise
+      ])
+      expect(text).toBe('Username or password wrong')
     })
+  
+    test('check that login using username works', async ({ page }) => {
+      await page.goto('https://localhost:3000/');
+      await page.locator('id=login').click();
+  
+      await page.locator('input#login').fill('username');
+      await page.locator('input#password').fill('password');
+  
+      let dialogPromise;
+      let text;
+      await Promise.all([
+        dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+          dialog.accept()
+          return dialog.message();
+        }),
+        page.locator('button:has-text("LOGIN")').click(),
+        text = await dialogPromise
+      ])
+      expect(text).toBe('Username or password wrong')
+    })
+  
+    test('check that login using email works', async ({ page }) => {
+      await page.goto('https://localhost:3000/');
+      await page.locator('id=login').click();
+  
+      await page.waitForSelector('#emailOption')
+      await page.locator('#emailOption').check();
+      await page.locator('input#login').fill('email');
+      await page.locator('input#password').fill('password');
+  
+      let dialogPromise;
+      let text;
+      await Promise.all([
+        dialogPromise = page.waitForEvent('dialog').then(async (dialog) => {
+          dialog.accept()
+          return dialog.message();
+        }),
+        page.locator('button:has-text("LOGIN")').click(),
+        text = await dialogPromise
+      ])
+      expect(text).toBe('Email or password wrong')
+    })
+  });
+
 })
-
