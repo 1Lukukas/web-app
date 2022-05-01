f11e476468aae2e8add276479cb161214eb5d3dc
1Lukukas <88420914+1Lukukas@users.noreply.github.com>
2022-05-01 13:06:53 +0300

    split app into app and server

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+const app = require('./app')(process.env.DATABASE_URL)
+const https = require('https');
+const fs = require('fs');
+const key = fs.readFileSync('key.pem');
+const cert = fs.readFileSync('cert.pem');
+const server = https.createServer({key: key, cert: cert }, app);
+
+server.listen(3000, () => { console.log('listening on 3000') });
