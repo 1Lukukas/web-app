5ed621750c73397b9bb377f0c5bef7250acbf90b
Edvinas Gaubas <e.gaubas18@gmail.com>
2022-05-01 14:29:19 +0300

    Add tests for records

--- routes/records.js
+++ routes/records.js
@@ -9,8 +9,6 @@ router.use('/css', express.static(path.join(__dirname, '/css')))
 router.use('/static', express.static(path.join(__dirname, '/static')))
 
 urlencodedParser = express.urlencoded({extended : false})
-const cookieParser = require('cookie-parser');
-router.use(cookieParser());
 // Getting all
 
 router.get('/all2', authenticateToken, async (req, res) => {
@@ -24,41 +22,41 @@ router.get('/all2', authenticateToken, async (req, res) => {
   }
 })
 
-router.get('/all', async (req, res) => {
-  try {
-    const records = await Record.find()
-    res.json(records)
-  } catch (err) {
-    res.status(500).json({ message: err.message })
-  }
-})
+// router.get('/all', async (req, res) => {
+//   try {
+//     const records = await Record.find()
+//     res.json(records)
+//   } catch (err) {
+//     res.status(500).json({ message: err.message })
+//   }
+// })
 
 // Getting One
-router.get('/:id', getRecord, (req, res) => {
-  res.status(204).json(res.record)
-})
+// router.get('/:id', getRecord, (req, res) => {
+//   res.status(204).json(res.record)
+// })
 
-router.get("/:id/details", getRecord, async(req, res) => {
-  res.render("detailsRecord.ejs", {data : res.record})
-})
+// router.get("/:id/details", getRecord, async(req, res) => {
+//   res.render("detailsRecord.ejs", {data : res.record})
+// })
 
 // Creating one
-router.post('/create', urlencodedParser, async (req, res) => {
-  const record = new Record({
-    amount: req.body.amount,
-    currency: req.body.currency,
-    category: req.body.category,
-    description: req.body.description,
-    recordType: req.body.recordType,
-    recordDate: req.body.recordDate
-  })
-  try {
-    const newRecord = await record.save()
-    res.status(204).json(newRecord)
-  } catch (err) {
-    res.status(400).json({ message: err.message })
-  }
-})
+// router.post('/create', urlencodedParser, async (req, res) => {
+//   const record = new Record({
+//     amount: req.body.amount,
+//     currency: req.body.currency,
+//     category: req.body.category,
+//     description: req.body.description,
+//     recordType: req.body.recordType,
+//     recordDate: req.body.recordDate
+//   })
+//   try {
+//     const newRecord = await record.save()
+//     res.status(204).json(newRecord)
+//   } catch (err) {
+//     res.status(400).json({ message: err.message })
+//   }
+// })
 
 router.post('/create2', urlencodedParser, authenticateToken, async (req, res) => {
   //console.log(req.user)
@@ -87,61 +85,61 @@ router.post('/create2', urlencodedParser, authenticateToken, async (req, res) =>
 })
 
 // Pass id to edit record page
-router.get("/:id/edit", getRecord, async(req, res) => {
-  res.render("editRecord.ejs", {data : res.record})
-})
+// router.get("/:id/edit", getRecord, async(req, res) => {
+//   res.render("editRecord.ejs", {data : res.record})
+// })
 
 // Updating One
-router.patch('/:id', getRecord, async (req, res) => {
-  if (req.body.amount != null) {
-    res.record.amount = req.body.amount
-  }
-  if (req.body.currency != null) {
-    res.record.currency = req.body.currency
-  }
-  if (req.body.currency != null) {
-    res.record.category = req.body.category
-  }
-  if (req.body.description != null) {
-    res.record.description = req.body.description
-  }
-  if (req.body.recordType != null) {
-    res.record.recordType = req.body.recordType
-  }
-  if (req.body.recordDate != null) {
-    res.record.recordDate = req.body.recordDate
-  }
-  try {
-    const updatedRecord = await res.record.save()
-    res.status(204).json(updatedRecord)
-  } catch (err) {
-    res.status(400).json({ message: err.message })
-  }
-})
+// router.patch('/:id', getRecord, async (req, res) => {
+//   if (req.body.amount != null) {
+//     res.record.amount = req.body.amount
+//   }
+//   if (req.body.currency != null) {
+//     res.record.currency = req.body.currency
+//   }
+//   if (req.body.currency != null) {
+//     res.record.category = req.body.category
+//   }
+//   if (req.body.description != null) {
+//     res.record.description = req.body.description
+//   }
+//   if (req.body.recordType != null) {
+//     res.record.recordType = req.body.recordType
+//   }
+//   if (req.body.recordDate != null) {
+//     res.record.recordDate = req.body.recordDate
+//   }
+//   try {
+//     const updatedRecord = await res.record.save()
+//     res.status(204).json(updatedRecord)
+//   } catch (err) {
+//     res.status(400).json({ message: err.message })
+//   }
+// })
 
 // Deleting One
-router.delete('/:id', getRecord, async (req, res) => {
-  try {
-    await res.record.remove()
-    res.message(`Record ${res.record._id} removed successfully`)
-  } catch (err) {
-    res.status(500).json({ message: err.message })
-  }
-})
+// router.delete('/:id', getRecord, async (req, res) => {
+//   try {
+//     await res.record.remove()
+//     res.message(`Record ${res.record._id} removed successfully`)
+//   } catch (err) {
+//     res.status(500).json({ message: err.message })
+//   }
+// })
 
-async function getRecord(req, res, next) {
-  let record
-  try {
-    record = await Record.findById(req.params.id)
-    if (record == null) {
-      return res.status(404).json({ message: 'Cannot find record' })
-    }
-  } catch (err) {
-    return res.status(500).json({ message: err.message })
-  }
+// async function getRecord(req, res, next) {
+//   let record
+//   try {
+//     record = await Record.findById(req.params.id)
+//     if (record == null) {
+//       return res.status(404).json({ message: 'Cannot find record' })
+//     }
+//   } catch (err) {
+//     return res.status(500).json({ message: err.message })
+//   }
 
-  res.record = record
-  next()
-}
+//   res.record = record
+//   next()
+// }
 
 module.exports = router
--- test/app.test.js
+++ test/app.test.js
@@ -3,72 +3,146 @@ const { response } = require('express')
 const request = require('supertest')
 const app = require('../app')(process.env.TEST_DATABASE_URL)
 const User = require('../models/user')
+const Record = require('../models/record')
 
-describe('Users API', () => {
-    beforeEach(async () => {
-        await User.deleteMany({})
-    });
+describe('Test suite', () => {
 
-    afterEach(async () => {
-        await User.deleteMany({})
-    });
+    describe('Users API', () => {
+        beforeEach(async () => {
+            await User.deleteMany({})
+        });
 
-    it('POST /users/login', async () => {
-        await request(app).post('/users/create')
-        .send('email=test&username=test&password=test')
-        .expect(204)
-    });
+        afterEach(async () => {
+            await User.deleteMany({})
+        });
 
-    it('GET /users/all', async () => {
-        const response = await request(app).get('/users/all')
-        .expect(200)
-        expect(response.body).toEqual([]);
-        //with data here
-    });
+        it('POST /users/login', async () => {
+            await request(app).post('/users/create')
+            .send('email=test&username=test&password=test')
+            .expect(204)
+        });
 
-    it('POST /users/create', async () =>{
-        const user = await new User({
-            email: "test",
-            username: "test",
-            password: "test",
-          }).save()
+        it('GET /users/all', async () => {
+            const response = await request(app).get('/users/all')
+            .expect(200)
+            expect(response.body).toEqual([]);
+            //with data here
+        });
 
-        const response = await request(app).post('/users/login')
-        .send({
-            username: "test",
-            email: "test",
-            password: "test"
+        it('POST /users/create', async () =>{
+            const user = await new User({
+                email: "test",
+                username: "test",
+                password: "test",
+            }).save()
+
+            const response = await request(app).post('/users/login')
+            .send({
+                username: "test",
+                email: "test",
+                password: "test"
+            })
+            .expect(200)
+            expect(response.body.accessToken).toBeTruthy();
         })
-        .expect(200)
-        expect(response.body.accessToken).toBeTruthy();
+
+        it('GET /users/me', async () =>{
+            const user = await new User({
+                email: "test",
+                username: "test",
+                password: "test",
+            }).save()
+
+            let response = await request(app).post('/users/login')
+            .send({
+                username: "test",
+                email: "test",
+                password: "test"
+            })
+            .expect(200)
+            expect(response.body.accessToken).toBeTruthy();
+
+            response = await request(app).get('/users/me')
+            .set('Authorization', `Bearer ${response.body.accessToken}`)
+            .expect(200)
+
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                username: 'test',
+                email: 'test',
+                password: 'test'
+                })
+            );
+        })
     })
 
-    it('POST /users/me', async () =>{
-        const user = await new User({
-            email: "test",
-            username: "test",
-            password: "test",
-          }).save()
+    describe("Records API", () =>{
 
-        let response = await request(app).post('/users/login')
-        .send({
-            username: "test",
-            email: "test",
-            password: "test"
-        })
-        .expect(200)
-        expect(response.body.accessToken).toBeTruthy();
+        beforeEach(async () => {
+            await User.deleteMany({})
+            await Record.deleteMany({})
+        });
 
-        response = await request(app).get('/users/me')
-        .set('Authorization', `Bearer ${response.body.accessToken}`)
-        .expect(200)
+        afterEach(async () => {
+            await User.deleteMany({})
+            await Record.deleteMany({})
+        });
 
-        expect(response.body).toEqual(
-            expect.objectContaining({
-              username: 'test',
-              email: 'test',
-              password: 'test'
+        it('POST records/create2', async () => {
+            const user = await new User({
+                email: "test",
+                username: "test",
+                password: "test",
+            }).save()
+
+            let response = await request(app).post('/users/login')
+            .send({
+                username: "test",
+                email: "test",
+                password: "test"
             })
-        );
+            .expect(200)
+            expect(response.body.accessToken).toBeTruthy();
+
+            response = await request(app).post('/records/create2')
+            .set('Authorization', `Bearer ${response.body.accessToken}`)
+            .send({amount:"222", recordType:"expense"})
+            .expect(204)
+        })
+
+        it('GET records/all2', async () =>{
+            const user = await new User({
+                email: "test",
+                username: "test",
+                password: "test",
+            }).save()
+
+            let response = await request(app).post('/users/login')
+            .send({
+                username: "test",
+                email: "test",
+                password: "test"
+            })
+            .expect(200)
+            expect(response.body.accessToken).toBeTruthy();
+            const accessToken = response.body.accessToken
+
+            response = await request(app).post('/records/create2')
+            .set('Authorization', `Bearer ${accessToken}`)
+            .send({amount:"222", recordType:"expense"})
+            .expect(204)
+
+            response = await request(app).get('/records/all2')
+            .set('Authorization', `Bearer ${accessToken}`)
+            .expect(200)
+            .expect('Content-Type', /json/)
+                expect(response.body).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        amount: expect.any(Number),
+                        recordType: expect.any(String)
+                    })
+                ])
+            );
+        })
     })
 })
